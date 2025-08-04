496b3dbe70e790f8130d9cea9c82fd8eacd0aa8b
Aayush Kumar <39685487+akumar2408@users.noreply.github.com>
2025-08-03 19:54:39 -0700

    Add Profile component

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>This page will allow users to view and update their account information.</p>
+    </div>
+  );
+};
+
+export default Profile;
